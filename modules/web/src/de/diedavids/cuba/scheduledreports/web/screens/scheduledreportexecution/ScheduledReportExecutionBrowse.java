96747983c7b09a37d925bb3c7666e56319447f1d
Mario <mario@die-davids.de>
2019-07-10 08:41:07 +0200

    make files in exec browse downloadable

--- modules/web/src/de/diedavids/cuba/scheduledreports/web/screens/scheduledreportexecution/ScheduledReportExecutionBrowse.java
+++ modules/web/src/de/diedavids/cuba/scheduledreports/web/screens/scheduledreportexecution/ScheduledReportExecutionBrowse.java
@@ -1,11 +1,26 @@
 package de.diedavids.cuba.scheduledreports.web.screens.scheduledreportexecution;
 
+import com.haulmont.cuba.core.entity.Entity;
+import com.haulmont.cuba.gui.export.ExportDisplay;
 import com.haulmont.cuba.gui.screen.*;
 import de.diedavids.cuba.scheduledreports.entity.ScheduledReportExecution;
+
+import javax.inject.Inject;
 
 @UiController("ddcsr_ScheduledReportExecution.browse")
 @UiDescriptor("scheduled-report-execution-browse.xml")
 @LookupComponent("scheduledReportExecutionsTable")
 @LoadDataBeforeShow
 public class ScheduledReportExecutionBrowse extends StandardLookup<ScheduledReportExecution> {
+
+
+    @Inject
+    protected ExportDisplay exportDisplay;
+
+    public void downloadFile(Entity item, String columnId) {
+
+        ScheduledReportExecution execution = (ScheduledReportExecution) item;
+
+        exportDisplay.show(execution.getReportFile());
+    }
 }
